69c3e9b9f17a64fb8afeb053023085d833ee7831
ShompiFlen <shompire02@gmail.com>
2023-12-02 04:35:32 -0300

    Comando para consultar los detalles de un torneo

---
+++ src/commands/consult-tournament.ts
@@ -0,0 +1,115 @@
+import { Subcommand } from "@sapphire/plugin-subcommands"
+import { Tournament, TournamentModel, TournamentStatus } from "../sequelize/index.js"
+import { Colors, EmbedBuilder } from "discord.js"
+
+export class TournamentCommands extends Subcommand {
+
+	public constructor(context: Subcommand.LoaderContext, options: Subcommand.Options) {
+		super(context, {
+			...options,
+			name: 'torneo',
+			subcommands: [
+				{
+					name: "detalles",
+					chatInputRun: "chatInputDetalles"
+				},
+			]
+		})
+	}
+
+
+	public override registerApplicationCommands(registry: Subcommand.Registry) {
+		registry.registerChatInputCommand((builder) => {
+			builder.setName("torneo")
+				.setDescription('Varios comandos relacionados con los torneos')
+				.setDMPermission(false)
+				.addSubcommand(details =>
+					details.setName('detalles')
+						.setDescription('Ve la información de un torneo')
+						.addIntegerOption(id =>
+							id.setName('id-torneo')
+								.setDescription('La id numérica del torneo')
+								.setMaxValue(100000)
+								.setMinValue(1)
+						)
+						.addStringOption(name =>
+							name.setName('nombre-torneo')
+								.setDescription('El nombre del torneo')
+								.setAutocomplete(true)
+								.setMaxLength(255)
+						)
+				)
+		}, { idHints: ["1179715303735832646"] })
+
+
+	}
+	public async chatInputDetalles(interaction: Subcommand.ChatInputCommandInteraction) {
+		// Your code goes here
+
+		const idTorneo = interaction.options.getInteger('id-torneo') ?? interaction.options.getString('nombre-torneo');
+
+		if (!idTorneo) return void await interaction.reply({ content: "Debes usar este comando con almenos un argumento, la id numérica o el nombre del torneo.\nLa id tiene prioridad por sobre el nombre.\nSi intentaste buscar un torneo por su nombre, asegurate de esperar a que se muestren las opciones en la opción `nombre-torneo` y escogerla desde las opciones, ya que la búsqueda se hará con el nombre completo del torneo.", ephemeral: true })
+
+		const torneo = await SearchTournament(+idTorneo)
+
+		if (!torneo) return void await interaction.reply({ content: 'No se encontró ningun torneo en la base de datos con los datos ingresados.', ephemeral: true })
+
+		return void await SendDetails(interaction, torneo);
+
+	} // End ChatInputRun
+
+	public async autocompleteRun(interaction: Subcommand.AutocompleteInteraction) {
+
+		const subcommand = interaction.options.getSubcommand()
+
+		if (subcommand === 'detalles') return void await detallesAutocomplete(interaction)
+	}
+}
+
+async function detallesAutocomplete(interaction: Subcommand.AutocompleteInteraction) {
+	const focusedOption = interaction.options.getFocused(true)
+
+	if (focusedOption.name === 'nombre-torneo') {
+		/** This should be okay for now, since we will probably not have too many tournaments stored */
+		const torneos = await TournamentModel.findAll()
+
+		return void await interaction.respond(
+			torneos.filter(torneo => torneo.name.toLowerCase().includes(
+				focusedOption.value.toLowerCase()
+			)).map(torneo => ({ name: torneo.name, value: torneo.id.toString() }))
+		)
+	}
+
+}
+
+async function SearchTournament(id: number) {
+	return await TournamentModel.findOne({ where: { id } })
+}
+
+export function TournamentDetailsEmbed(torneo: Tournament) {
+
+	const players = JSON.parse(torneo.players) as string[]
+
+	return (
+		new EmbedBuilder()
+			/** This probably will need change if later i want to implement more statuses. */
+			.setTitle(`${torneo.name} (${torneo.status === 0 ? "CLOSED" : "OPEN"})`)
+			.setDescription(
+				`**Organizado por**: <@${torneo.organized_by}>` +
+				`\n**Juego**: ${torneo.game}` +
+				`${torneo.is_tr_capped ? `\n**TR CAP**: ${torneo.tr_cap}` : ""}` +
+				`${torneo.is_rank_capped ? `\n**RANK CAP**: ${torneo.rank_cap}` : ""}` +
+				`${torneo.is_country_locked ? `\n**COUNTRY LOCK**: ${torneo.country_lock}` : ""}` +
+				`\n**Jugadores registrados**: ${players.length}`
+			)
+			.setColor(Colors.White)
+			.setTimestamp()
+	)
+}
+
+async function SendDetails(interaction: Subcommand.ChatInputCommandInteraction, torneo: Tournament) {
+	return void await interaction.reply({
+		content: `Aquí está la información del torneo **${torneo.name}**`,
+		embeds: [TournamentDetailsEmbed(torneo)]
+	})
+}
