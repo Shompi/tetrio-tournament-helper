8c50470ae9a21cc37b41a3ad84fb3eb24b7da23a
ShompiFlen <shompire02@gmail.com>
2023-12-04 22:49:28 -0300

    Reorganized code

--- src/helper-functions/index.ts
+++ src/helper-functions/index.ts
@@ -2,7 +2,10 @@
 * This file will contain tetrio api function calls
 * and maybe other stuff.
 */
+import { EmbedBuilder, Colors } from "discord.js";
 import { request } from "undici"
+import { Tournament } from "../sequelize/index.js";
+import { PlayerModel } from "../sequelize/index.js";
 
 export type TetrioApiCacheStatus = "hit" | "miss" | "awaited"
 export type TetrioUserRole = "anon" | "user" | "bot" | "halfmod" | "mod" | "admin" | "sysop" | "banned"
@@ -121,3 +124,84 @@ export function GetUserProfileURL(username: string) {
 
 export function TournamentIsJoinableByTetrioPlayer(playerData: TetrioUserData, caps: { rank_cap?: string; tr_cap?: number; country_lock?: string }) {
 }
+
+export function TournamentDetailsEmbed(torneo: Tournament) {
+
+	const players = torneo.players
+	console.log(`[DEBUG] PLAYERS ARRAY =>`, players)
+
+	return (
+		new EmbedBuilder()
+			/** This probably will need change if later i want to implement more statuses. */
+			.setTitle(`${torneo.name} (${torneo.status === 0 ? "CLOSED" : "OPEN"})`)
+			.setDescription(
+				`**ID del torneo**: ${torneo.id}` +
+				`\n**Organizado por**: <@${torneo.organized_by}>` +
+				`\n**Juego**: ${torneo.game}` +
+				`${torneo.is_tr_capped ? `\n**TR CAP**: ${torneo.tr_cap}` : ""}` +
+				`${torneo.is_rank_capped ? `\n**RANK CAP**: ${torneo.rank_cap}` : ""}` +
+				`${torneo.is_country_locked ? `\n**COUNTRY LOCK**: ${torneo.country_lock}` : ""}` +
+				`\n**Jugadores registrados**: ${players.length}`
+			)
+			.setColor(Colors.White)
+			.setTimestamp()
+	)
+}
+
+export async function AddTetrioPlayerToDatabase({ discordId, tetrioId }: { discordId: string; tetrioId: string; }, userData: TetrioUserData) {
+
+	if (!discordId || !tetrioId)
+		throw new Error(`Missing one of the arguments. dId: ${discordId}, tId: ${tetrioId}`);
+
+
+	console.log("[DEBUG] Añadiendo nuevo PLAYER a la base de datos...");
+
+	await PlayerModel.create({
+		discord_id: discordId,
+		tetrio_id: tetrioId,
+		data: userData
+	});
+
+	console.log(`[PLAYERS DATABASE] => Player (${discordId}) - ${tetrioId} se ha guardado en la base de datos.`);
+
+}
+export async function RunTetrioTournamentRegistrationChecks(userData: TetrioUserData, torneo: Tournament, discordId: string): Promise<{ allowed: boolean; reason?: string; }> {
+	// In here we have to check for Tetrio caps like rank, rating and country lock and if the player is already on the tournament.
+	if (torneo.players.includes(discordId)) {
+		return ({ allowed: false, reason: "Ya te encuentras en la lista de participantes de este torneo." });
+	}
+
+	if (torneo.is_country_locked && torneo.country_lock?.toUpperCase() !== userData.user.country?.toUpperCase()) {
+		// The country of the player doesn't match the tournament country lock
+		return ({ allowed: false, reason: "El pais del jugador es distinto al pais del torneo." });
+	}
+
+	if (torneo.is_tr_capped) {
+		if (userData.user.league.rank === 'z')
+			return ({ allowed: false, reason: "El jugador no posee un rank actualmente." });
+
+
+		if (userData.user.league.rating > torneo.tr_cap!) {
+			return ({ allowed: false, reason: "El rating del jugador está por sobre el limite de TR del torneo." });
+		}
+	}
+
+	if (torneo.is_rank_capped) {
+
+		if (userData.user.league.rank === 'z')
+			return ({ allowed: false, reason: "El jugador es actualmente UNRANKED en Tetra League." });
+
+		const tournamentRankIndex = TetrioRanksArray.findIndex((rank) => rank === torneo.rank_cap);
+		const userRankIndex = TetrioRanksArray.findIndex((rank) => rank === userData.user.league.rank);
+
+		if (tournamentRankIndex < userRankIndex)
+			return ({ allowed: false, reason: "El rank del jugador está por sobre el límite de rank impuesto por el torneo." });
+	}
+
+	if (torneo.max_players && torneo.players.length >= torneo.max_players) {
+		return ({ allowed: false, reason: "El torneo ha alcanzado el máximo de participantes." });
+	}
+
+	return { allowed: true };
+}
+
