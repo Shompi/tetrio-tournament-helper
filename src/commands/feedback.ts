7316b84ee85b6dd07a2dc0bc760f2ce6e0bf3474
ShompiFlen <shompire02@gmail.com>
2023-12-10 02:49:20 -0300

    Comando de feedback (No implementado)

---
+++ src/commands/feedback.ts
@@ -0,0 +1,33 @@
+import { Command } from "@sapphire/framework"
+
+
+export class FeedbackCommand extends Command {
+
+
+	public constructor(context: Command.LoaderContext, options: Command.Options) {
+		super(context, { ...options });
+	}
+
+
+	public override registerApplicationCommands(registry: Command.Registry) {
+		registry.registerChatInputCommand((builder) => {
+			builder.setName("feedback")
+				.setDMPermission(false)
+				.setDescription('Envia una sugerencia respecto al bot!')
+				.addStringOption(message =>
+					message.setName('mensaje')
+						.setDescription('El mensaje que quieres enviar a los desarrolladores')
+						.setRequired(true)
+						.setMinLength(10)
+						.setMaxLength(2000)
+				)
+
+		}, { idHints: [""] })
+
+
+	}
+	public async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+		// Your code goes here
+		return void await interaction.reply({ content: 'Este comando aún no está implementado.', ephemeral: true })
+	}
+}
