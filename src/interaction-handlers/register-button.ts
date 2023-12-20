1307ef14d51f6a146f18e8c3a775238714153d14
ShompiFlen <shompire02@gmail.com>
2023-12-20 05:53:41 -0300

    changed method for getting tournament from guild

--- src/interaction-handlers/register-button.ts
+++ src/interaction-handlers/register-button.ts
@@ -1,7 +1,7 @@
 import { InteractionHandler, InteractionHandlerTypes } from '@sapphire/framework';
 import { ActionRowBuilder, ButtonBuilder, ButtonInteraction, ButtonStyle, ComponentType, ModalBuilder, TextInputBuilder, TextInputStyle } from "discord.js"
 import { Tournament, TournamentModel, TournamentStatus } from '../sequelize/Tournaments.js';
-import { GetPlayerFromDatabase, GetUserDataFromTetrio, TetrioUserProfileEmbed } from '../helper-functions/index.js';
+import { GetPlayerFromDatabase, GetTournamentFromGuild, GetUserDataFromTetrio, TetrioUserProfileEmbed } from '../helper-functions/index.js';
 import { TournamentDetailsEmbed } from "../helper-functions/index.js";
 import { AddTetrioPlayerToDatabase } from '../helper-functions/index.js';
 import { RunTetrioTournamentRegistrationChecks } from '../helper-functions/index.js';
@@ -15,20 +15,23 @@ export class RegisterButtonHandler extends InteractionHandler {
 	public async run(interaction: ButtonInteraction<'cached'>, tournamentId: string) {
 
 
-		const torneo = await TournamentModel.findOne({
-			where: {
-				id: +tournamentId
-			}
-		})
+		const tournament = await GetTournamentFromGuild(interaction.guildId, +tournamentId)
 
-		if (!torneo) return void await interaction.reply({ content: 'Este torneo no existe.', ephemeral: true });
+		if (!tournament) return void await interaction.reply({ content: 'Este torneo no existe.', ephemeral: true });
 
-		if (torneo.status === TournamentStatus.CLOSED) return void await interaction.reply({ content: 'No puedes inscribirte en este torneo por que ya ha cerrado.', ephemeral: true })
+		if (tournament.status === TournamentStatus.CLOSED) return void await interaction.reply({ content: 'No puedes inscribirte en este torneo por que ya ha cerrado.', ephemeral: true })
 
-		if (torneo.game === "TETRIO") {
-			return void await HandleTetrioRegistration(interaction, torneo);
+		if (tournament.game === "TETRIO") {
+			return void await HandleTetrioRegistration(interaction, tournament);
 		}
 
+		// If the tournament is from a game that does not track player stats then we can just add this player to the db
+		// Although I wanna add something like an SR question / modal to help seeding Tetris Effect Tournaments and add it into the data prop.
+
+		return void await AddPlayerToTournamentPlayerList(tournament, {
+			discordId: interaction.user.id,
+			challongeId: null,
+		})
 	}
 
 	public parse(interaction: ButtonInteraction) {
@@ -44,61 +47,8 @@ export class RegisterButtonHandler extends InteractionHandler {
 
 async function HandleTetrioRegistration(interaction: ButtonInteraction<'cached'>, tournament: Tournament) {
 
-	// Check if the player is already in our database
-	const playerData = await GetPlayerFromDatabase(interaction.user.id)
-
-	if (!playerData) {
-		void await HandleNewPlayerRegistration(interaction, tournament)
-	} else {
-		const check = await RunTetrioTournamentRegistrationChecks(playerData.data, tournament, interaction.user.id)
-
-		if (!check.allowed) {
-			return void await interaction.reply({
-				content: `No te puedes inscribir en este torneo.\nRazón: ${check.reason}`,
-				ephemeral: true
-			})
-		}
-
-		void await AddPlayerToTournamentPlayerList(tournament, interaction.user.id, playerData.challonge_id)
-		void await interaction.reply({ content: '✅ !Has sido añadido exitosamente al torneo!', ephemeral: true })
-	}
-	// Update the message with the new details
-	return void await interaction.message.edit({
-		embeds: [TournamentDetailsEmbed(tournament)]
-	})
-}
-
-async function HandleNewPlayerRegistration(interaction: ButtonInteraction<'cached'>, tournament: Tournament): Promise<void> {
-
-	/** Interaction has not been deferred at this point */
-
 	// show the modal
-	const TetrioModal = new ModalBuilder()
-		.setCustomId(`${interaction.user.id}-tetrio-profile`)
-		.setTitle(`Inscripción para torneo de TETRIO`)
-		.addComponents(
-			new ActionRowBuilder<TextInputBuilder>()
-				.setComponents(
-					new TextInputBuilder()
-						.setStyle(TextInputStyle.Short)
-						.setCustomId('tetrio-username')
-						.setLabel("Tu username de TETRIO")
-						.setPlaceholder("USERNAME")
-						.setMaxLength(50)
-						.setMinLength(1)
-						.setRequired(true),
-				),
-			new ActionRowBuilder<TextInputBuilder>()
-				.setComponents(
-					new TextInputBuilder()
-						.setStyle(TextInputStyle.Short)
-						.setCustomId('challonge-username')
-						.setLabel('Tu username de challonge (Opcional)')
-						.setPlaceholder("TetrisMaster_123")
-						.setMaxLength(50)
-						.setRequired(false)
-				)
-		)
+	const TetrioModal = BuildTetrioModal(interaction)
 
 	await interaction.showModal(TetrioModal)
 
@@ -110,29 +60,24 @@ async function HandleNewPlayerRegistration(interaction: ButtonInteraction<'cache
 	).catch(() => null);
 
 	// Interaction expired.
-	if (!modalSubmition) return;
-
-	await modalSubmition.deferReply({ ephemeral: true })
+	if (!modalSubmition) return null;
 
 	const tetrioUsername = modalSubmition.fields.getTextInputValue('tetrio-username')
-	let challongeUsername: string | null = modalSubmition.fields.getTextInputValue('challonge-username')
+	let challongeUsername = modalSubmition.fields.getTextInputValue('challonge-username')
 
-	const userData = await GetUserDataFromTetrio(tetrioUsername)
+	const playerdata = await GetUserDataFromTetrio(tetrioUsername)
 
-	if (!userData) return void await modalSubmition.editReply({ content: `No encontré a ningún usuario de TETRIO con el username ${tetrioUsername}. Asegúrate de escribirlo correctamente y presiona el botón de inscripción nuevamente para volver a comenzar.` })
+	if (!playerdata) return void await modalSubmition.reply({
+		content: `El jugador ${tetrioUsername} no existe. Verifica que hayas escrito el nombre de manera correcta.`,
+		ephemeral: true
+	})
 
-	const profileEmbed = TetrioUserProfileEmbed(userData)
+	const profileEmbed = TetrioUserProfileEmbed(playerdata)
 
 	const confirmButton = new ButtonBuilder()
 		.setCustomId(`t-profile-confirm`)
 		.setLabel("Si")
 		.setEmoji("✅")
-		.setStyle(ButtonStyle.Secondary)
-
-	const retryButton = new ButtonBuilder()
-		.setCustomId(`t-profile-retry`)
-		.setLabel("No")
-		.setEmoji("❌")
 		.setStyle(ButtonStyle.Secondary)
 
 	const cancelButton = new ButtonBuilder()
@@ -141,7 +86,7 @@ async function HandleNewPlayerRegistration(interaction: ButtonInteraction<'cache
 		.setStyle(ButtonStyle.Danger)
 
 	const confirmOrDenyRow = new ActionRowBuilder<ButtonBuilder>()
-		.setComponents(confirmButton, retryButton)
+		.setComponents(confirmButton)
 
 	const cancelRow = new ActionRowBuilder<ButtonBuilder>()
 		.setComponents(cancelButton)
@@ -160,11 +105,13 @@ async function HandleNewPlayerRegistration(interaction: ButtonInteraction<'cache
 
 	}).catch(() => null);
 
-	if (!pressedButton) return void await profileReply.edit({
-		content: 'La interacción ha expirado, si quieres continuar con el proceso, por favor presiona el boton de inscripción nuevamente.',
-		components: [],
-		embeds: [],
-	})
+	if (!pressedButton) {
+		return void await profileReply.edit({
+			content: 'La interacción ha expirado, si quieres continuar con el proceso, por favor presiona el boton de inscripción nuevamente.',
+			components: [],
+			embeds: [],
+		})
+	}
 
 	if (pressedButton.customId === "t-profile-cancel") {
 		return void await pressedButton.update({
@@ -174,41 +121,108 @@ async function HandleNewPlayerRegistration(interaction: ButtonInteraction<'cache
 		})
 	}
 
-	if (pressedButton.customId === "t-profile-retry") {
-		// We basically need to run all this process again.
-		return void HandleNewPlayerRegistration(pressedButton, tournament)
-	}
-
 	if (pressedButton.customId === 't-profile-confirm') {
+		// Run tournament checks
+		const check = await RunTetrioTournamentRegistrationChecks(playerdata, tournament, interaction.user.id)
 
-		if (challongeUsername.length === 0)
-			challongeUsername = null
-
-		void await AddTetrioPlayerToDatabase({ discordId: interaction.user.id, tetrioId: userData.username, challongeId: challongeUsername }, userData)
-
-		const result = await RunTetrioTournamentRegistrationChecks(userData, tournament, interaction.user.id)
-
-		if (!result.allowed) {
-			return void await interaction.update({
-				content: `No puedes inscribirte en este torneo.\nRazón: ${result.reason}`,
+		if (!check.allowed) {
+			return void await interaction.reply({
+				content: `No te puedes inscribir en este torneo.\nRazón: ${check.reason}`,
+				ephemeral: true
 			})
 		}
+
+		void await AddPlayerToTournamentPlayerList(tournament, {
+			discordId: interaction.user.id,
+			challongeId: challongeUsername,
+			data: playerdata
+		})
+
+		void await interaction.reply({ content: '✅ !Has sido añadido exitosamente al torneo!', ephemeral: true })
+		// Update the message with the new details
+		return void await interaction.message.edit({
+			embeds: [TournamentDetailsEmbed(tournament)]
+		})
 	}
+}
 
-	// User passed the checks and can be added to the tournament
-	void await AddPlayerToTournamentPlayerList(tournament, interaction.user.id, challongeUsername)
+type TetrioPlayerModalInfo = {
+	username: string
+	challongeId: string | null
+}
 
-	console.log("[ACTION ON PLAYER] Chequeando si hay roles para agregar...");
 
-	// Add corresponding roles to the player
-	if (tournament.add_roles.length > 0) {
-		await pressedButton.member.roles.add(tournament.add_roles)
-		console.log(`[ACTION ON PLAYER] Se le agregaron ${tournament.add_roles.length} roles al jugador ${interaction.user.id}`);
-	}
+function BuildGeneralRegistrationModal(interaction: ButtonInteraction<'cached'>) {
+	return new ModalBuilder()
+		.setCustomId(`${interaction.user.id}-registration`)
+		.setTitle(`Inscripción para torneo`)
+		.addComponents(
+			new ActionRowBuilder<TextInputBuilder>()
+				.setComponents(
+					new TextInputBuilder()
+						.setStyle(TextInputStyle.Short)
+						.setCustomId('challonge-username')
+						.setLabel('Tu username de challonge (Opcional)')
+						.setPlaceholder("TetrisMaster_123")
+						.setMaxLength(50)
+						.setRequired(false)
+				)
+		);
+}
 
-	return void await pressedButton.update({
-		content: `Te has inscrito en el torneo **${tournament.name}** exitósamente!`,
-		components: [],
-		embeds: []
-	})
+function BuildTetrisEffectModal(interaction: ButtonInteraction<'cached'>) {
+	return new ModalBuilder()
+		.setCustomId(`${interaction.user.id}-registration`)
+		.setTitle(`Inscripción para torneo de Tetris Effect`)
+		.addComponents(
+			new ActionRowBuilder<TextInputBuilder>()
+				.setComponents(
+					new TextInputBuilder()
+						.setStyle(TextInputStyle.Short)
+						.setCustomId('challonge-username')
+						.setLabel("Tu username de Challonge o Email")
+						.setPlaceholder("...")
+						.setMaxLength(50)
+						.setRequired(false)
+				),
+			new ActionRowBuilder<TextInputBuilder>()
+				.setComponents(
+					new TextInputBuilder()
+						.setStyle(TextInputStyle.Short)
+						.setCustomId('tec-skillrate')
+						.setLabel('Tu SR / RH / RATE')
+						.setPlaceholder("1234")
+						.setMaxLength(10)
+						.setRequired(false)
+				)
+		);
+}
+
+function BuildTetrioModal(interaction: ButtonInteraction<"cached">) {
+	return new ModalBuilder()
+		.setCustomId(`${interaction.user.id}-registration`)
+		.setTitle(`Inscripción para torneo de TETRIO`)
+		.addComponents(
+			new ActionRowBuilder<TextInputBuilder>()
+				.setComponents(
+					new TextInputBuilder()
+						.setStyle(TextInputStyle.Short)
+						.setCustomId('tetrio-username')
+						.setLabel("Tu username de TETRIO")
+						.setPlaceholder("USERNAME")
+						.setMaxLength(50)
+						.setMinLength(1)
+						.setRequired(true)
+				),
+			new ActionRowBuilder<TextInputBuilder>()
+				.setComponents(
+					new TextInputBuilder()
+						.setStyle(TextInputStyle.Short)
+						.setCustomId('challonge-username')
+						.setLabel('Tu username de challonge (Opcional)')
+						.setPlaceholder("TetrisMaster_123")
+						.setMaxLength(50)
+						.setRequired(false)
+				)
+		);
 }
