ae726044e1785053b5dc580845fce6418e94cf8e
ShompiFlen <shompire02@gmail.com>
2023-12-22 05:49:05 -0300

    Errors templates

---
+++ src/helper-functions/common-errors.ts
@@ -0,0 +1,6 @@
+export const CommonErrors = {
+	InvalidTournamentId: "❌ La id del torneo que has ingresado no pertenece a este servidor o no existe.\nSi usas el nombre del torneo, asegúrate de que selecciones una de las opciones del **autocompletado**.",
+	GuildTournamentNotFound: "❌ No encontré ningún torneo con la id que ingresaste en este servidor.",
+	TournamentNotEditable: '❌ No puedes editar la información de este torneo por que está marcado como **FINALIZADO**',
+	NotImplemented: "⛔ Esta opción aún no ha sido implementada.",
+}
