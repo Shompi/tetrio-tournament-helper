9a5906ed7ca9f61c84d6796c7f6d1378613985a0
ShompiFlen <shompire02@gmail.com>
2023-12-27 04:01:35 -0300

    renamed

---
+++ src/helper-functions/common-messages.ts
@@ -0,0 +1,15 @@
+export const CommonMessages = {
+	/** @deprecated */
+	InvalidTournamentId: "❌ La id del torneo que has ingresado no pertenece a este servidor o no existe.\nSi usas el nombre del torneo, asegúrate de que selecciones una de las opciones del **autocompletado**.",
+	GuildTournamentNotFound: "❌ No encontré ningún torneo con la id que ingresaste en este servidor\nSi usas el nombre del torneo, asegurate de usar una de las opciones del autocompletado.",
+	TournamentNotEditable: '❌ No puedes editar la información de este torneo por que está marcado como **FINALIZADO**',
+	TournamentNotJoinable: '❌ No te puedes inscribir en este torneo por que está marcado como **Cerrado** o **Finalizado**.',
+	TournamentNotLeaveable: '❌ No te puedes desinscribir de este torneo por que ya está cerrado.',
+	PlayerNotRegistered: '⚠️ No estás inscrito/a en este torneo.',
+	CheckInNotAllowed: '⚠️ No puedes hacer Check-in en este torneo por que ya ha finalizado.',
+	AlreadyCheckedIn: '⚠️ Ya estás en la lista de Checked-in.',
+	NotImplemented: "⚠️ Esta opción aún no ha sido implementada.",
+	UserIsBlocked: "❌ No tienes permitido usar este comando.",
+	UserWhitelisted: `✅ El usuario {username} puede utilizar los comandos del bot nuevamente.`,
+}
+
