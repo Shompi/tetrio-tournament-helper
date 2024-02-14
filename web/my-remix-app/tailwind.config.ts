c20af83790ee56c4cd44a42679e94fae39391ce8
ShompiFlen <shompire02@gmail.com>
2024-02-14 03:46:06 -0300

    Added nexa font family

--- web/my-remix-app/tailwind.config.ts
+++ web/my-remix-app/tailwind.config.ts
@@ -3,7 +3,11 @@ import type { Config } from 'tailwindcss'
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nexa: ['"Nexa"', "sans-serif"]
+      }
+    },
   },
   plugins: [],
 } satisfies Config
