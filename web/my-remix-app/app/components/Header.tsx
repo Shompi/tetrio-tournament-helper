797bbffff014f6d5f50faeaf18a0e5d70a6b21b4
ShompiFlen <shompire02@gmail.com>
2024-03-03 07:25:02 -0300

    Added animations and new section on the header

--- web/my-remix-app/app/components/Header.tsx
+++ web/my-remix-app/app/components/Header.tsx
@@ -2,19 +2,17 @@ import { Link } from "@remix-run/react";
 
 export default function Header() {
 	return (
-		<header className="flex flex-row justify-between bg-slate-700 text-slate-300 px-32">
-			<div className="p-4">
-				<p className="font-semibold text-4xl">Index</p>
+		<header className="flex flex-row justify-around text-slate-300 min-h-8">
+			<div className="p-4 flex flex-col justify-center">
+				<p className="font-semibold text-2xl font-nexa">HOME</p>
 			</div>
 
 			<div className="flex flex-col justify-center">
-				<ul className="flex flex-row text-2xl font-semibold gap-x-4">
-					<li className="list-item">
-						<Link className="hover:text-slate-600" to={"/about"}>About</Link>
-					</li>
+				<ul className="flex text-xl font-semibold h-full items-center">
+					<Link className="list-item hover:animate-pulse hover:bg-slate-200 h-full p-4 hover:text-slate-600" to={"/docs"}>DOCS</Link>
+					<Link className="list-item hover:animate-pulse hover:bg-slate-200 h-full p-4 hover:text-slate-600" to={"/about"}>ABOUT</Link>
 				</ul>
 			</div>
-
 		</header>
 	)
 }
