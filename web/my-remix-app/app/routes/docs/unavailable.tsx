20b5822f354a0adb4845d5fbcc660d060da4abca
ShompiFlen <shompire02@gmail.com>
2024-03-03 07:26:09 -0300

    Unavailable page if the api does not respond

---
+++ web/my-remix-app/app/routes/docs/unavailable.tsx
@@ -0,0 +1,7 @@
+export default function Unavailable() {
+	return (
+		<main className="py-12 text-white">
+			<p className="text-4xl font-nexa">❌ API is unavailable at this moment.</p>
+		</main>
+	)
+}
