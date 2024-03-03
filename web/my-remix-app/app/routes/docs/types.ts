ca3854999515172c3ed6fd9e16ae083481b30b45
ShompiFlen <shompire02@gmail.com>
2024-03-03 07:25:56 -0300

    Type for API response

---
+++ web/my-remix-app/app/routes/docs/types.ts
@@ -0,0 +1,24 @@
+export type StatsAPIResponse = {
+	error: boolean,
+	message?: string,
+	is_ready: boolean,
+	username: string | null,
+	display_name: string | null,
+	tag: string | null,
+	avatar: string|null,
+	uptime: number | null,
+	average_ping: number,
+	hex_color: `#${string}` | null,
+	/** @link https://discord-api-types.dev/api/discord-api-types-v10/enum/UserFlags */
+	flags: string[] | null,
+	stats: {
+		guild_count: number,
+		user_count: number,
+		tournament_count: number,
+		emoji_count: number,
+		// shard_status: unknown,
+		verified: boolean
+	},
+	/** Im too lazy to type this shaiza */
+	commands: unknown[]
+}
